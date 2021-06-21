e643be9a763d3a4ef2b821f61c8fb996994e351e
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2021-06-21 13:28:58 +0200

    docs: :memo: update Readme

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { getInput, debug, setFailed, setOutput } from '@actions/core';
+import { getInput, debug, setFailed, setOutput, info, error } from '@actions/core';
 
 const token = getInput('token') || process.env.GH_PAT || process.env.GITHUB_TOKEN;
 
 export const run = async () => {
   if (!token) throw new Error('GitHub token not found');
   const ms: string = getInput('milliseconds');
-  debug(`Waiting ${ms} milliseconds ...`);
+  info(`Waiting ${ms} milliseconds ...`);
 
   debug(new Date().toTimeString());
   await wait(parseInt(ms, 10));
@@ -18,7 +18,7 @@ export const wait = (milliseconds: number) => {
   return new Promise<void>((resolve) => setTimeout(() => resolve(), milliseconds));
 };
 
-run().catch((error) => {
-  console.error('ERROR', error);
-  setFailed(error.message);
+run().catch((err) => {
+  error(err);
+  setFailed(err.message);
 });
